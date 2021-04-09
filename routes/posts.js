c360b1cf3e34abe61a7e7edb9b457554f2a5460a
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-09 13:41:29 +0530

    Setting Authentication for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
 
 let router = express.Router();
 
 let postController = require('../controllers/post_controller');
 
-router.post('/create', postController.create);
+router.post('/create',passport.checkAuthentication , postController.create);
 
 module.exports = router;

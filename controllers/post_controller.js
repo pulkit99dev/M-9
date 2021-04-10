3936b4b00feb77148c0a4b70e3a712620078d1ce
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-10 10:30:17 +0530

    delete post & data related to it

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../model/post");
+const Comment = require('../model/comment')
 
 module.exports.create = (req, res)=>{
     Post.create({
@@ -13,4 +14,18 @@ module.exports.create = (req, res)=>{
         res.redirect('back');
     }
     )
+}
+
+module.exports.destroy = (req, res)=>{
+    Post.findByIdAndDelete(req.params.id, function(err, post){
+        if(post.user == req.user.id){
+            post.remove();
+        
+            Comment.deleteMany({post : req.params.id}, function(err){
+                return res.redirect('/');
+            })
+        }else{
+            return res.redirect('/')
+        }
+    })
 }

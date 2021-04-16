4869d83c3e89f97158d30f56ad83e2608750576e
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-16 15:31:37 +0530

    Displaying Post data on the page

--- assets/js/home-posts.js
+++ assets/js/home-posts.js
@@ -1,26 +1,58 @@
 {
-    // method to submit form data using AJAX
-    let createPost = function(){
-        let newPostForm = $('#new-post-form')
+  // method to submit form data using AJAX
+  let createPost = function () {
+    let newPostForm = $("#new-post-form");
 
-        newPostForm.submit(function(e){
-            e.preventDefault();
+    newPostForm.submit(function (e) {
+      e.preventDefault();
 
-            // Manually Submitting the post creating request in AJAX
-            $.ajax({
-                type: 'post',
-                url: '/post/create',
-                data: newPostForm.serialize(),
-                success: function(data){
-                    console.log(data);
-                }, error: function(error){
-                    console.log(error.resposeText)
-                }
-            })
-
-        })
-    }
-    createPost();
+      // Manually Submitting the post creating request in AJAX
+      $.ajax({
+        type: "post",
+        url: "/post/create",
+        data: newPostForm.serialize(),
+        success: function (data) {
+          let newPost = newPostDom(data.data.post);
+          $('#post-list-container>ul').prepend(newPost)
+        },
+        error: function (error) {
+          console.log(error.resposeText);
+        },
+      });
+    });
+  };
+  createPost();
 }
 
 // Method to create Post using DOM
+
+let newPostDom = function (post) {
+  return $(`
+    <li id="post-${post._id}">
+    <p>${post.content}</p>
+    <small>${post.user.name}</small>
+
+        <a class="delete-post-button" href="/post/destroy/${post.id}">XXXXX</a>
+
+  <div class="post-comment">
+    <form action="/comment/create" method="POST">
+      <input
+        type="text"
+        name="content"
+        placeholder="Type Here to add comment..."
+        required
+      />
+      <input type="hidden" name="post" value="${post._id}" />
+      <input type="submit" value="Add Comment" />
+    </form>
+
+    <div class="post-comment-list">
+      <ul id="post-comment-${post._id}">
+      
+      </ul>
+    </div>
+  </div>
+</li>
+
+    `);
+};

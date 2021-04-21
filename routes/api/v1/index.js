14acafd567f80ebbf8bb93435eca700fd7665b26
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-21 11:56:29 +0530

    v1 comments

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -5,5 +5,7 @@ const router = express.Router();
 // router.use('/v1', require('./v1')); 
 
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
+
 
 module.exports = router;

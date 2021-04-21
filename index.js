6fffc3eac632be91d1b736866c459b41eae9b9f1
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-21 15:04:29 +0530

    jwt strategy

--- index.js
+++ index.js
@@ -10,19 +10,22 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
+const passportJwt = require("./config/passport-jwt-strategy"); 
 const MongoStore = require("connect-mongo")(session);
 
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const custoMware = require('./config/middleware-flash');
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const custoMware = require("./config/middleware-flash");
 
-app.use(sassMiddleware({
-  src: './assets/scss',
-  dest: './assets/css',
-  debug: true,
-  outputStyle: 'extended',
-  prefix:'/css'
-}))
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -30,7 +33,7 @@ app.use(express.static("./assets"));
 app.use(expressLayouts);
 
 //make the upload path available to the browser or template engines
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // Extract styles & scripts from subpages
 app.set("layout extractStyles", true);
@@ -51,23 +54,19 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 1000,
     },
-    store: new MongoStore(
-      {
-        mongooseConnection : db,
-        autoRemove: 'disabled'
-      }
-    )
+    store: new MongoStore({
+      mongooseConnection: db,
+      autoRemove: "disabled",
+    }),
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.use(passport.setAuthenticatedUser);
 app.use(flash());
 app.use(custoMware.setFlash);
-
 
 //Setting up router
 app.use("/", require("./routes"));

7c328e03925ab8758aeab9012c581319dca2b5f5
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-21 11:41:22 +0530

    making api versions

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const postApi = require('../../../controllers/api/v1/posts_api')
+
 // router.use('/v1', require('./v1')); 
 
-router.use('/posts', require('./posts'));
+router.get('/', postApi.index);
 
 module.exports = router;

1ab9f57d5dbee4ccab0ebbf16013e58fc89b986e
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-04-21 16:07:09 +0530

    setting up JWT post req

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,26 @@
+const User = require("../../../model/user");
+const jwt = require("jsonwebtoken");
+
+module.exports.createSession = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (!user || user.password != req.body.password) {
+      return res.json(422, {
+        message: "Invalid user or password",
+      });
+    }
+
+    return res.json(200, {
+      message: "Sign in successful, here is token",
+      data: {
+        token: jwt.sign(user.toJSON(), "Connecttie", { expiresIn: 10000 }),
+      },
+    });
+  } catch (err) {
+    console.log("****", err);
+    return res.json(500, {
+      message: "internal Server Error",
+    });
+  }
+};
